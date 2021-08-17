7a8e24a9515c08bf67c55dd0faa2dd5cf03dab00
victor miguel <victormigueltejeda@gmail.com>
2021-08-17 09:12:17 -0600

    quitando console.log del archivi index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Template from "./templates/Template.js";
 import "./styles/main.css";
 import "./styles/vars.styl";
 
-console.log("hola")(async function App() {
+(async function App() {
   const main = null || document.getElementById("main");
   main.innerHTML = await Template();
 })();
